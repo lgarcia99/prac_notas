d860a542ef69f9ec174c40da8b8b51a6fc29dfc2
Luis García <lgarcia174099@alumno.itson.edu.mx>
2020-05-01 21:29:42 -0700

    add delete functionality

--- app/src/main/java/garcia/luis/misnotas/AgregarNotaActivity.kt
+++ app/src/main/java/garcia/luis/misnotas/AgregarNotaActivity.kt
@@ -1,12 +1,92 @@
 package garcia.luis.misnotas
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_agregar_nota.*
+import android.Manifest
+import android.os.Environment
+import android.widget.Toast
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.Exception
 
 class AgregarNotaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_nota)
+
+        btn_guardar.setOnClickListener {
+            guardar_nota()
+        }
+    }
+
+    fun guardar_nota() {
+        //verifica que tenga los permisos
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            == PackageManager.PERMISSION_DENIED
+        ) {
+            // si no los tiene, los pide al usuario
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                235
+            )
+            //si tiene permisos, procede a guardar
+        } else {
+            guardar()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            235 -> {
+                //pregunta si el usuario aceptó los permisos
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    guardar()
+                } else {
+                    // si no aceptó, coloca un mensaje
+                    Toast.makeText(this, "Error: permisos denegados", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    public fun guardar() {
+        var titulo = et_titulo.text.toString()
+        var cuerpo = et_contenido.text.toString()
+
+        if (titulo == "" || cuerpo == "") {
+            Toast.makeText(this, "Error: campos vacíos", Toast.LENGTH_SHORT).show()
+        } else {
+            try {
+                val archivo = File(ubicacion(), titulo + ".txt")
+                val fos = FileOutputStream(archivo)
+                fos.write(cuerpo.toByteArray())
+                fos.close()
+                Toast.makeText(
+                    this,
+                    "Se guardó el archivo en la carpeta pública",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } catch (e: Exception) {
+                Toast.makeText(this, "Error: No se guardó el archivo", Toast.LENGTH_SHORT).show()
+            }
+        }
+        finish()
+    }
+
+    private fun ubicacion(): String {
+        val carpeta = File(Environment.getExternalStorageDirectory(), "notas")
+        if (!carpeta.exists()) carpeta.mkdir()
+
+        return carpeta.absolutePath
     }
 }
--- app/src/main/java/garcia/luis/misnotas/MainActivity.kt
+++ app/src/main/java/garcia/luis/misnotas/MainActivity.kt
@@ -1,14 +1,19 @@
 package garcia.luis.misnotas
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.nota_layout.view.*
+import java.io.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     var notas = ArrayList<Nota>()
@@ -18,16 +23,69 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        notasDePrueba()
+        fab.setOnClickListener {
+            var intent = Intent(this, AgregarNotaActivity::class.java)
+            startActivityForResult(intent, 123)
+        }
+
+        leerNotas()
 
         adaptador = AdapterNotas(this, notas)
         list_view.adapter = adaptador
     }
 
-    fun notasDePrueba() {
-        notas.add(Nota("prueba 1", "contenido de la nota 1"))
-        notas.add(Nota("prueba 2", "contenido de la nota 2"))
-        notas.add(Nota("prueba 3", "contenido de la nota 3"))
+    fun leerNotas() {
+        notas.clear()
+        var carpeta = File(ubicacion().absolutePath)
+
+        if (carpeta.exists()) {
+            var archivos = carpeta.listFiles()
+            if (archivos != null) {
+                for (archivo in archivos) {
+                    leerArchivo(archivo)
+                }
+            }
+        }
+    }
+
+    fun leerArchivo(archivo: File) {
+        val fis = FileInputStream(archivo)
+        val di = DataInputStream(fis)
+        val br = BufferedReader(InputStreamReader(di))
+        var strLine: String? = br.readLine()
+        var myData = ""
+
+        //lee los archivos almacenados en la memoria
+        while (strLine != null) {
+            myData = myData + strLine
+            strLine = br.readLine()
+        }
+        br.close()
+        di.close()
+        fis.close()
+
+        //elimina el .txt
+        var nombre = archivo.name.substring(0, archivo.name.length-4)
+        //crea la nota y la agrega a la lista
+        var nota = Nota(nombre, myData)
+        notas.add(nota)
+    }
+
+    private fun ubicacion(): File {
+        val carpeta = File(Environment.getExternalStorageDirectory(), "notas")
+        if (!carpeta.exists()) carpeta.mkdir()
+
+        return carpeta
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        //se activa cuando regresa del AgregarNotaActivity
+        if (requestCode == 123) {
+            leerNotas()
+            adaptador.notifyDataSetChanged()
+        }
     }
 
     class AdapterNotas : BaseAdapter {
@@ -47,7 +105,36 @@ class MainActivity : AppCompatActivity() {
             vista.tv_titulo_det.text = nota.titulo
             vista.tv_contenido_det.text = nota.contenido
 
+            vista.boton_borrar.setOnClickListener {
+                eliminar(nota.titulo)
+                notas.remove(nota)
+                this.notifyDataSetChanged()
+            }
+
             return vista
+        }
+
+        private fun eliminar(titulo: String) {
+            if (titulo == "") {
+                Toast.makeText(context, "Error: título vacío", Toast.LENGTH_SHORT).show()
+            } else {
+                try {
+                    //elimina el archivo con el título seleccionado
+                    val archivo = File(ubicacion(), titulo + ".txt")
+                    archivo.delete()
+
+                    Toast.makeText(context, "Se eliminó el archivo", Toast.LENGTH_SHORT).show()
+                } catch (e: Exception) {
+                    Toast.makeText(context,"Error al eliminar el archivo", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        private fun ubicacion(): String {
+            val album = File(Environment.getExternalStorageDirectory(), "notas")
+            if (!album.exists()) album.mkdir()
+
+            return album.absolutePath
         }
 
         override fun getItem(position: Int): Any {
